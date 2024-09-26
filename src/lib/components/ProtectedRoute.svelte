0737aff235dd79b270e70503fd5e30da68693f75
Pritom-Paul <p.paulprem@gmail.com>
2024-09-26 13:21:57 +0600

    turn of auth

--- src/lib/components/ProtectedRoute.svelte
+++ src/lib/components/ProtectedRoute.svelte
@@ -3,21 +3,21 @@
     import { goto } from '$app/navigation';
     import { writable } from 'svelte/store';
 
-    const isAuthenticated = writable(false);
+    const isAuthenticated = writable(true);
 
-    onMount(() => {
-        const token = window.localStorage.getItem('access_token');
-        if (token) {
-            isAuthenticated.set(true);
-        } else {
-            // Redirect to the login page with the original route
-            const currentPath = window.location.pathname;
-            goto(`/login?redirect=${encodeURIComponent(currentPath)}`);
-        }
-    });
+    // onMount(() => {
+    //     const token = window.localStorage.getItem('access_token');
+    //     if (token) {
+    //         isAuthenticated.set(true);
+    //     } else {
+    //         // Redirect to the login page with the original route
+    //         const currentPath = window.location.pathname;
+    //         goto(`/login?redirect=${encodeURIComponent(currentPath)}`);
+    //     }
+    // });
 </script>
 
-{#if $isAuthenticated}
-    <slot />
+<slot />
+<!-- {#if $isAuthenticated} -->
 <!-- Optionally, show a loading state while checking -->
-{/if}
+<!-- {/if} -->
